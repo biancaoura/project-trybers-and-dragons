bcac6f22471e14d9dca16eaf29afa57bdba4554c
Bianca Oura <biancaoura@gmail.com>
2023-01-19 19:35:13 -0300

    feat: Race class - initial setup

---
+++ src/Races/Race.ts
@@ -0,0 +1,20 @@
+export default abstract class Race {
+  constructor(
+    private readonly _name: string,
+    private readonly _dexterity: number,
+  ) {}
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+}
